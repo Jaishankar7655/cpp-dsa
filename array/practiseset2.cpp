b7af8dad992ce283f9e63d231fa0f067b156c770
jaishankar7655 <jaishankar7655@gmail.com>
2026-05-06 02:02:42 +0530

    fix the code in the practise set 2 file in array folder

--- array/practiseset2.cpp
+++ array/practiseset2.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 using namespace std;
-
 int linearSearch(int array[], int target, int size)
 {
     for (int i = 0; i < size; i++)
     {
         if (array[i] == target)
         {
-            cout << i;
+
+            return i;
         }
     }
     return -1;
 }
-
 int main()
 {
-    int target = 11;
+    int target = 12;
     int size = 8;
     int array[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    linearSearch(array, target, size);
+    int index = linearSearch(array, target, size);
+    if (index == -1)
+    {
+        cout << "Element not found in the array";
+    }
+    else
+    {
+        cout << "Element found at index : " << index;
+    }
 
     return 0;
 }
